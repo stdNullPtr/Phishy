0c40bc8191e88f84ce9544b2789837f9e81aec59
Antonio <antonio.liubchev@gmail.com>
2023-06-05 21:44:46 +0300

    Add comments to serialization

--- Phishy/Configs/Properties.cs
+++ Phishy/Configs/Properties.cs
@@ -12,12 +12,19 @@ namespace Phishy.Configs
 {
     public sealed class Properties
     {
+        [YamlMember(Description = "Keyboard key binding for fishing cast (Example: 1)")]
         public string KeyboardKeyStartFishing { get; set; }
+        [YamlMember(Description = "Keyboard key binding for applying lure (optional)")]
         public string KeyboardKeyApplyLure { get; set; }
+        [YamlMember(Description = "Keyboard key binding for applying a second lure (optional)")]
         public string? KeyboardKeyApplySecondLure { get; set; }
+        [YamlMember(Description = "Buff duration of first lure (mandatory if first lure keybind is set)")]
         public int LureBuffDurationMinutes { get; set; }
+        [YamlMember(Description = "Buff duration of second lure (mandatory if second lure keybind is set)")]
         public int? SecondLureBuffDurationMinutes { get; set; }
+        [YamlMember(Description = "Fishing cast duration in seconds")]
         public int FishingChannelDurationSeconds { get; set; }
+        [YamlMember(Description = "Window name of the game, when you hover over it in the taskbar")]
         public string GameWindowName { get; set; }
 
         public Properties()
