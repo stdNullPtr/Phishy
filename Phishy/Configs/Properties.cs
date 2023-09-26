eb242c9ebedd84d8123dcf2b5bbfc3bdbf55b315
Antonio <antonio.liubchev@gmail.com>
2023-09-27 01:59:42 +0300

    refactor

--- Phishy/Configs/Properties.cs
+++ Phishy/Configs/Properties.cs
@@ -8,34 +8,33 @@ using System.Threading.Tasks;
 using Phishy.Utils;
 using YamlDotNet.Serialization;
 
-namespace Phishy.Configs
-{
-    public sealed class Properties
-    {
-        [YamlMember(Description = "Keyboard key binding for fishing cast (Example: 1)")]
-        public string KeyboardKeyStartFishing { get; set; }
-        [YamlMember(Description = "Keyboard key binding for applying lure (optional)")]
-        public string KeyboardKeyApplyLure { get; set; }
-        [YamlMember(Description = "Keyboard key binding for applying a second lure (optional)")]
-        public string? KeyboardKeyApplySecondLure { get; set; }
-        [YamlMember(Description = "Buff duration of first lure (mandatory if first lure keybind is set)")]
-        public int LureBuffDurationMinutes { get; set; }
-        [YamlMember(Description = "Buff duration of second lure (mandatory if second lure keybind is set)")]
-        public int? SecondLureBuffDurationMinutes { get; set; }
-        [YamlMember(Description = "Fishing cast duration in seconds")]
-        public int FishingChannelDurationSeconds { get; set; }
-        [YamlMember(Description = "Window name of the game, when you hover over it in the taskbar")]
-        public string GameWindowName { get; set; }
+namespace Phishy.Configs;
 
-        public Properties()
-        {
-            KeyboardKeyStartFishing = KeyboardUtils.ConvertToString(Keys.D1);
-            KeyboardKeyApplyLure = KeyboardUtils.ConvertToString(Keys.D2);
-            KeyboardKeyApplySecondLure = null;
-            LureBuffDurationMinutes = TimeSpan.FromMinutes(10).Minutes;
-            SecondLureBuffDurationMinutes = null;
-            FishingChannelDurationSeconds = TimeSpan.FromSeconds(20).Seconds;
-            GameWindowName = "Game Window Name";
-        }
+public sealed class Properties
+{
+    [YamlMember(Description = "Keyboard key binding for fishing cast (Example: 1)")]
+    public string KeyboardKeyStartFishing { get; set; }
+    [YamlMember(Description = "Keyboard key binding for applying lure (optional)")]
+    public string KeyboardKeyApplyLure { get; set; }
+    [YamlMember(Description = "Keyboard key binding for applying a second lure (optional)")]
+    public string? KeyboardKeyApplySecondLure { get; set; }
+    [YamlMember(Description = "Buff duration of first lure (mandatory if first lure keybind is set)")]
+    public int LureBuffDurationMinutes { get; set; }
+    [YamlMember(Description = "Buff duration of second lure (mandatory if second lure keybind is set)")]
+    public int? SecondLureBuffDurationMinutes { get; set; }
+    [YamlMember(Description = "Fishing cast duration in seconds")]
+    public int FishingChannelDurationSeconds { get; set; }
+    [YamlMember(Description = "Window name of the game, when you hover over it in the taskbar")]
+    public string GameWindowName { get; set; }
+
+    public Properties()
+    {
+        KeyboardKeyStartFishing = KeyboardUtils.ConvertToString(Keys.D1);
+        KeyboardKeyApplyLure = KeyboardUtils.ConvertToString(Keys.D2);
+        KeyboardKeyApplySecondLure = null;
+        LureBuffDurationMinutes = TimeSpan.FromMinutes(10).Minutes;
+        SecondLureBuffDurationMinutes = null;
+        FishingChannelDurationSeconds = TimeSpan.FromSeconds(20).Seconds;
+        GameWindowName = "Game Window Name";
     }
 }
